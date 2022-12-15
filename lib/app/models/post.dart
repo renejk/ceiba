a1993b13843c40c52876182de521252796d432ab
renejk <renejosecardonaherrera@gmail.com>
2022-12-15 15:13:36 -0500

    first commit

---
+++ lib/app/models/post.dart
@@ -0,0 +1,24 @@
+class Post {
+  String? userId;
+  String? id;
+  String? title;
+  String? body;
+
+  Post({this.userId, this.id, this.title, this.body});
+
+  Post.fromJson(Map<String, dynamic> json) {
+    userId = json['userId'];
+    id = json['id'];
+    title = json['title'];
+    body = json['body'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final data = <String, dynamic>{};
+    data['userId'] = userId;
+    data['id'] = id;
+    data['title'] = title;
+    data['body'] = body;
+    return data;
+  }
+}
